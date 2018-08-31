fe4478fa644105217f13224b8da320437aebddd7
Ahmed Nergal <ahmed.nergal@gmail.com>
2018-08-31 15:13:39 +0200

    Cleaned the code a little

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-console.log("HEEEEEEEEEEY GUYS");
 var existingAntennas = []
-var textInput = document.querySelector("#height");
+var textInput = document.querySelector("#antenna-height");
 var submit = document.querySelector("#submit");
 var iterator = document.querySelector("#iterator");
 var ul = document.querySelector(".list");
@@ -11,26 +10,28 @@ var initButton = document.querySelector("#init");
 var nextLocationsList = document.querySelector(".available-locations");
 towerHeight = 0
 
+// Adds an existing antenna to the existing antenna list in the HTML
 function addAnother(antenna){
   var li = document.createElement("li");
   li.appendChild(document.createTextNode(antenna));
   ul.appendChild(li);
 }
 
+// Enters the tower height to the HTML and to the script
 towerHeightSubmit.addEventListener("click", function(){
     towerHeight = Number(towerHeightText.value);
     currentTowerHeight.textContent = towerHeight.toString()
 })
 
-
+// Submits the antenna heights and pushes them to the existingAntennas list
 submit.addEventListener("click", function(){
   numAntenna = Number(textInput.value)
-  console.log(numAntenna);
   addAnother(textInput.value);
   existingAntennas.push(numAntenna);
   console.log(typeof(numAntenna));
 });
 
+// Initiates the calculation script and creates a list of the avaliable antenna locations
 init.addEventListener("click", function(){
     availableLocationsList = next_antenna_locator(towerHeight, existingAntennas);
     console.log(availableLocationsList);
@@ -41,18 +42,15 @@ init.addEventListener("click", function(){
     });
 })
 
+// Brute forces through the tower heights and finds the available locations to install the antenna conforming to specs
 function next_antenna_locator(towerHeight, existingAntennas){
   validPoints = [];
-  antennaRanges = {};
 
   existingAntennas.forEach(function(antenna){
-    antennaRanges.base = antenna;
-    antennaRanges.range = [antenna, antenna + 4];
 
     });
     validCounter = 0;
     nextAntennaLocation = towerHeight - 2.6;
-    nextAntennaRange = [towerHeight, towerHeight + 4];
 
     while (nextAntennaLocation > 0){
       existingAntennas.forEach(function(antenna){
@@ -66,7 +64,7 @@ function next_antenna_locator(towerHeight, existingAntennas){
         validPoints.push(nextAntennaLocation);
       }
       validCounter = 0
-      nextAntennaLocation -= 0.25
+      nextAntennaLocation -= 0.10
     }
     return validPoints
   }
